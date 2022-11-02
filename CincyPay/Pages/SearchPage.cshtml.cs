f7c080ce0dc425773c442536d050cf5069bd12e5
lokeshrs <lokesh.rs12@gmail.com>
2022-11-01 20:44:47 -0400

    Landing Page First Version with demo data

---
+++ CincyPay/Pages/SearchPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CincyPay.Pages
+{
+    public class SearchPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
