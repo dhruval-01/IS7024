9ad0f2e9515441bbfb9765ecce2b90fa77cbb50c
DHruval <abc@xyz.com>
2022-11-05 00:48:04 -0400

    Implemented Employee w/ salaries dataset Json data parsing

--- CincyPay/Pages/Index.cshtml.cs
+++ CincyPay/Pages/Index.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmpSalaryData;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CincyPay.Pages
 {
     public class IndexModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +17,17 @@ namespace CincyPay.Pages
 
         public void OnGet()
         {
+            var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/wmj4-ygbf.json");
+            HttpResponseMessage result = task.Result;
+            List<EmpSalary> salaryData = new List<EmpSalary>();
+            if (result.IsSuccessStatusCode) 
+            { 
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                salaryData = EmpSalary.FromJson(jsonString);
+            }
+
+            ViewData["EmpSalary"] = salaryData;
 
         }
     }
