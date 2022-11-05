74b92f01c8b3d57d751eb864e4d352c51cafa3a5
lokeshrs <lokesh.rs12@gmail.com>
2022-11-05 19:18:06 -0400

    JSON parsing to Search Page

--- CincyPay/Pages/SearchPage.cshtml.cs
+++ CincyPay/Pages/SearchPage.cshtml.cs
@@ -1,12 +1,49 @@
+using EmpSalaryData;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace CincyPay.Pages
 {
     public class SearchPageModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<SearchPageModel> _logger;
+
+        public SearchPageModel(ILogger<SearchPageModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
+            var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/wmj4-ygbf.json");
+            HttpResponseMessage result = task.Result;
+            List<EmpSalary> salaryData = new List<EmpSalary>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("empsalary.json"));
+                JArray jsonArray = JArray.Parse(jsonString);
+                IList<string> validationEvents = new List<string>();
+                if (jsonArray.IsValid(schema, out validationEvents))
+                {
+                    salaryData = EmpSalary.FromJson(jsonString);
+                }
+                else
+                {
+                    foreach (string evt in validationEvents)
+                    {
+                        Console.WriteLine(evt);
+                    }
+
+                }
+
+            }
+
+            ViewData["EmpSalary"] = salaryData;
         }
     }
 }
