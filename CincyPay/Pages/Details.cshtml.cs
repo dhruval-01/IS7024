28706ea5bfd3e680490c5823c61ba7a612ae4220
adarshjoshi023 <adarshjoshi.kodapganti5@gmail.com>
2022-11-09 14:50:28 -0500

    Modifies the .cs file to get the department list as well

--- CincyPay/Pages/Details.cshtml.cs
+++ CincyPay/Pages/Details.cshtml.cs
@@ -13,12 +13,6 @@ namespace CincyPay.Pages
         static readonly HttpClient client = new HttpClient();
         public void OnGet()
         {
-            List<EmpSalary> FinalFilteredList = GetFilteredDeptData();
-
-            ViewData["EmpSalary"] = FinalFilteredList;
-        }
-        private static List<EmpSalary> GetFilteredDeptData()
-        {
             var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/wmj4-ygbf.json");
             HttpResponseMessage result = task.Result;
             List<EmpSalary> salaryData = new List<EmpSalary>();
@@ -45,13 +39,22 @@ namespace CincyPay.Pages
             }
             Task<HttpResponseMessage> deptTask = client.GetAsync("https://data.cincinnati-oh.gov/resource/txnn-6e6x.json");
             HttpResponseMessage deptResult = deptTask.Result;
-            Task<string> deptTaskString = deptResult.Content.ReadAsStringAsync();
-            string deptJson = deptTaskString.Result;
+            List<Department> depList = new List<Department>();
+          
+                Task<string> deptTaskString = deptResult.Content.ReadAsStringAsync();
+                string deptJson = deptTaskString.Result;
+                depList=Department.FromJson(deptJson);
+
+
             List<Department> departments = Department.FromJson(deptJson);
+            
 
             IDictionary<string, Department> filterByDept = new Dictionary<string, Department>();
+
             foreach (Department department in departments)
             {
+                depList.Add(department);
+
                 filterByDept[department.DepartmentCodes] = department;
             }
             List<EmpSalary> FinalFilteredList = new List<EmpSalary>();
@@ -65,7 +68,63 @@ namespace CincyPay.Pages
             {
 
             }
-            return FinalFilteredList;
+
+            ViewData["EmpSalary"] = FinalFilteredList;
+            ViewData["depList"] = depList;
+
+
         }
+        //private static List<EmpSalary> GetFilteredDeptData()
+        //{
+        //    var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/wmj4-ygbf.json");
+        //    HttpResponseMessage result = task.Result;
+        //    List<EmpSalary> salaryData = new List<EmpSalary>();
+        //    if (result.IsSuccessStatusCode)
+        //    {
+        //        Task<string> readString = result.Content.ReadAsStringAsync();
+        //        string jsonString = readString.Result;
+        //        JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("empsalary.json"));
+        //        JArray jsonArray = JArray.Parse(jsonString);
+        //        IList<string> validationEvents = new List<string>();
+        //        if (jsonArray.IsValid(schema, out validationEvents))
+        //        {
+        //            salaryData = EmpSalary.FromJson(jsonString);
+        //        }
+        //        else
+        //        {
+        //            foreach (string evt in validationEvents)
+        //            {
+        //                Console.WriteLine(evt);
+        //            }
+
+        //        }
+
+        //    }
+        //    Task<HttpResponseMessage> deptTask = client.GetAsync("https://data.cincinnati-oh.gov/resource/txnn-6e6x.json");
+        //    HttpResponseMessage deptResult = deptTask.Result;
+        //    Task<string> deptTaskString = deptResult.Content.ReadAsStringAsync();
+        //    string deptJson = deptTaskString.Result;
+        //    List<Department> departments = Department.FromJson(deptJson);
+
+        //    IDictionary<string, Department> filterByDept = new Dictionary<string, Department>();
+            
+        //    foreach (Department department in departments)
+        //    {
+
+        //        filterByDept[department.DepartmentCodes] = department;
+        //    }
+        //    List<EmpSalary> FinalFilteredList = new List<EmpSalary>();
+        //    foreach (EmpSalary empSalary in salaryData)
+        //    {
+        //        if (filterByDept.ContainsKey(empSalary.Jobcode))
+        //        {
+        //            FinalFilteredList.Add(empSalary);
+        //        }
+        //    }
+        //    {
+
+        //    }
+        //    return FinalFilteredList;
+        //}
     }
 }
