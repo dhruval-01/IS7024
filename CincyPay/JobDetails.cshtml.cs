bffd21d0f043309017219fda9dd0e85db012e836
DHruval <dhruval.chheda@gmail.com>
2022-11-09 19:14:16 -0500

    Added JobDetails razor page with table for selected attributes

---
+++ CincyPay/JobDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CincyPay
+{
+    public class JobDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
