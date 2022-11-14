f68a0e79dd44fe3f87b3f181d9a3039659058a2b
Christina Schwierling <parziacl@ucmail.uc.edu>
2022-11-14 11:48:29 -0500

    Removed the json call on the index.cshtml.cs page.  The call wasn't used on the home page.  It is used on the searchPage.cshtml.cs page.

--- CincyPay/Pages/Index.cshtml.cs
+++ CincyPay/Pages/Index.cshtml.cs
@@ -16,32 +16,5 @@ namespace CincyPay.Pages
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-            var task = client.GetAsync("https://data.cincinnati-oh.gov/resource/wmj4-ygbf.json");
-            HttpResponseMessage result = task.Result;
-            List<EmpSalary> salaryData = new List<EmpSalary>();
-            if (result.IsSuccessStatusCode) 
-            { 
-                Task<string> readString = result.Content.ReadAsStringAsync();
-                string jsonString = readString.Result;
-                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("empsalary.json"));
-                JArray jsonArray = JArray.Parse(jsonString);
-                IList<string> validationEvents = new List<string>();
-                if(jsonArray.IsValid(schema, out validationEvents))
-                {
-                    salaryData = EmpSalary.FromJson(jsonString);
-                } else {
-                    foreach(string evt in validationEvents) {
-                        Console.WriteLine(evt);
-                    }
-                       
-                }
-                
-            }
-
-            ViewData["EmpSalary"] = salaryData;
-
-        }
     }
 }
