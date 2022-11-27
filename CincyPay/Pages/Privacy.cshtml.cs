849cf9256112187dd4235e168bac09a5b829ebc4
DHruval <dhruval.chheda@gmail.com>
2022-11-27 00:24:44 -0500

    Added Movie.cs and json parsing of it

--- CincyPay/Pages/Privacy.cshtml.cs
+++ CincyPay/Pages/Privacy.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MovieData;
 
 namespace CincyPay.Pages
 {
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        static readonly HttpClient client = new HttpClient();
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
@@ -14,6 +16,18 @@ namespace CincyPay.Pages
 
         public void OnGet()
         {
+            var task = client.GetAsync("https://imdb-api.com/en/API/Top250Movies/k_thj97up3");
+            HttpResponseMessage result = task.Result;
+            Movie movielist = new Movie();
+            if (result.IsSuccessStatusCode) 
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonstring = readString.Result;
+                movielist = Movie.FromJson(jsonstring);
+            }
+
+            ViewData["MovieData"] = movielist;
+
         }
     }
 }
