dd70b4d0256d2cf4aad9019ed0af6704f722c49a
lokeshrs <lokesh.rs12@gmail.com>
2022-11-28 18:14:03 -0500

    Interests Page

---
+++ CincyPay/Pages/Interests.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CincyPay.Pages
+{
+    public class InterestsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
